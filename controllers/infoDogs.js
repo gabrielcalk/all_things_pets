08802d5d3d8f671cd81fa22b3634505891c6de5c
Brent Graves <graves.brent@gmail.com>
2021-11-02 18:38:04 -0400

    still needs routes to pass to JSON correctly have the data.

--- controllers/infoDogs.js
+++ controllers/infoDogs.js
@@ -1,9 +1,9 @@
 const infoDogsRouter = require('express').Router();
 const Dogs = require('../models');
 
-router.get('/', async (req, res) => {
+infoDogsRouter.get('/', async (req, res) => {
     try{
-      const product_data = await Product.findAll({
+      const dogs_data  = await dogs.findAll({
         include: [{model: Dogs} ]
       })
       res.status(200).json(dogs_data)
@@ -11,3 +11,14 @@ router.get('/', async (req, res) => {
       res.status(400).json(err)
     }
 });
+
+infoDogsRouter.post('/', async (req, res) => {
+  try {
+    const dogs_data = await dogs.create(req.body);
+    res.status(200).json(dogs_data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = infoDogsRouter;
