8e46e31231d33ec41444c3216e157740623cc592
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-02 18:41:27 -0400

    set up syntex for cat database

---
+++ db/cat.sql
@@ -0,0 +1,506 @@
+[
+    {
+      "breed": "Abyssinian ",
+      "specifics": {
+          "hypo-allergenic": "No", 
+          "grooming_frequency": "Minimal/Once a Month",
+          "like_children": "Yes",
+          "outdoor_indoor": "Outdoor",
+          "energy_level": "Energetic",
+          "color_options": ["Ginger/Red", "Blue/Gray", "Brown"],
+          "affectionate": "No",
+          "independence": "No",
+          "animal_companionship": ["Share home with another Cat", "Share home with another Dog"]
+      }
+    },
+
+    {
+      "breed": "American Bobtail",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "Minimal/Once a Month",
+          "like_children": "No",
+          "outdoor_indoor": "Outdoor",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "American Shorthair",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+  
+
+    {
+      "breed": "American Wirehair",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Balinese",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Bengal",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+      "breed": "Birman",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+   {
+      "breed": "Bombay",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+   {
+      "breed": "British Shorthair",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "British Longhair",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Burmese",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Cornish Rex",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Devon Rex",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Himalayan",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Japanese Bobtail",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Javanese",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Maine Coon",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+
+    {
+      "breed": "Manx",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+   {
+      "breed": "Munchkin",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Norwegian Forest",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Oriental Shorthair",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": " ",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Persian",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Ragamuffin ",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Ragdoll",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+    {
+      "breed": "Russian Blue",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Scottish Fold",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Siberian"",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Siamese",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Sphynx/Hairless Cat",
+      "specifics": {
+          "hypo-allergenic": "Yes", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Tonkinese",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Turkish Angora	",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+
+  {
+    "breed": "Turkish Van",
+      "specifics": {
+          "hypo-allergenic": "", 
+          "grooming_frequency": "",
+          "like_children": "",
+          "outdoor_indoor": "",
+          "energy_level": "",
+          "color_options": ["", "", ""],
+          "affectionate": "",
+          "independence": "",
+          "animal_companionship": ["", ""]
+      }
+    },
+  
+  ]
