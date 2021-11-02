5d83ff8fa642380d72f35ae5e064f9f0ac32f20b
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-11-02 13:40:39 -0400

    update syntex of cat questions

---
+++ public/js/catQuestions.js
@@ -0,0 +1,87 @@
+const catQuestions = [
+    {
+        question: 'Do you need a hypo-allergenic breed?',
+        options: [
+            'Yes',
+            'No'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'How much time can you spend grooming?',
+        options: [
+            'Minimal/Once per Month', 
+            'Once per Week'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'Will your animal be around children?',
+        options: [
+            'Yes',
+            'No'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'Will your ca be Outdoor or Indoor?',
+        options: [
+            'Outdoor', 
+            'Indoor'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'How much energy are you expecting your cat to have?',
+        options: [
+            'Very Energetic', 
+            'Docile'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'Do you want a cat that craves affection?',
+        options: [
+            'Yes',
+            'No'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'Do you need a cat that can be left alone for extended periods of time?',
+        options: [
+            'Yes',
+            'No'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'What color do you prefer your cat to be?',
+        options: [
+            'Black', 
+            'White', 
+            'Blue/Gray', 
+            'Ginger/Red', 
+            'Tortishell', 
+            'No Preference'
+        ],
+        answer: []
+    },
+
+    {
+        question: 'Do you currently have a veterinarian?',
+        options: [
+            'Yes',
+            'No, but I will find my own',
+            'No, but I would like some recommendations'
+        ]
+    }
+    
+];
