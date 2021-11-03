1e4e1f336f1b0366ff47ee31f29b977dbe35f2a4
Gabriel Cavalcante Causin <89816900+gabrielcalk@users.noreply.github.com>
2021-11-03 11:08:27 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Cats = require("./Cats");
 const Dogs = require("./Dogs");
-<<<<<<< HEAD
+
 
 // User.hasMany(Post, {
 //     foreignKey: "userId",
@@ -13,17 +13,5 @@ const Dogs = require("./Dogs");
 //     foreignKey: "userId"
 // });
 
-=======
-const dognames = require("./dognames")
-const catnames = require("./catnames")
-// User.hasMany(Post, {
-//     foreignKey: "userId",
-//     onDelete: "CASCADE"
-// });
 
-// Post.belongsTo(User, {
-//     foreignKey: "userId"
-// });
-
->>>>>>> 138d63f2928308b84ee86c3646586db869ba177d
 module.exports = { Cats, Dogs };
